4c34dab0fe561310dfca78c629083dd14a80c1a1
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-27 21:51:03 -0300

    create user mapper and implements

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/mapper/IUserMapper.java
@@ -0,0 +1,9 @@
+package com.heavydelay.BandsSync.Api.model.mapper;
+
+import com.heavydelay.BandsSync.Api.model.dto.user.UserResponseDto;
+import com.heavydelay.BandsSync.Api.model.entity.User;
+
+public interface IUserMapper {
+    UserResponseDto toBasicDto(User user);
+    UserResponseDto toDetailedDto(User user);
+}
