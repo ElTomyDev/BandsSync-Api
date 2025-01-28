6ee98ae7191d1c5159afba7f302fcaf9de472a25
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-28 14:56:08 -0300

    create role mapper and implements

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/mapper/impl/RoleImplMapper.java
@@ -0,0 +1,17 @@
+package com.heavydelay.BandsSync.Api.model.mapper.impl;
+
+import com.heavydelay.BandsSync.Api.model.dto.role.RoleResponseDto;
+import com.heavydelay.BandsSync.Api.model.entity.Role;
+import com.heavydelay.BandsSync.Api.model.mapper.IRoleMapper;
+
+public class RoleImplMapper implements IRoleMapper{
+
+    @Override
+    public RoleResponseDto toBasicDto(Role role) {
+        return RoleResponseDto.builder()
+               .idRole(role.getIdRole())
+               .roleName(role.getRoleName())
+               .build();
+    }
+
+}
