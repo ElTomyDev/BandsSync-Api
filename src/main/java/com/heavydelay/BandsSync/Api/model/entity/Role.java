50feff0b7cedaa9326331a94ecb8390f673503a5
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-28 14:45:00 -0300

    create Role entity

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/entity/Role.java
@@ -0,0 +1,30 @@
+package com.heavydelay.BandsSync.Api.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_role")
+    private Integer idRole;
+
+    @Column(name = "role_name")
+    @Size(max = 50)
+    private String roleName;
+
+}
