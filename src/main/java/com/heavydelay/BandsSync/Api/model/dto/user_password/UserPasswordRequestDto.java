4885a22952514cc72c8113ee7c12d50e12787978
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-30 17:12:21 -0300

    create password request dto

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/dto/user_password/UserPasswordRequestDto.java
@@ -0,0 +1,16 @@
+package com.heavydelay.BandsSync.Api.model.dto.user_password;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserPasswordRequestDto {
+
+    @NotBlank(message = "Password cannot be blank")
+    // falta validacion de contraseña personalizada
+    private String password;
+
+    @NotBlank(message = "Password reset token cannot be blank")
+    @Size(min = 10, max = 255, message = "Password reset token it must have at least 10 characters and a maximum of 255 characters.")
+    private String passwordResetToken;
+
+}
