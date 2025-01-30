25eb4dc77d7c7c738a850a31b4d73f3706a0a615
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-29 21:55:30 -0300

    create band member entity

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/entity/BandMember.java
@@ -0,0 +1,54 @@
+package com.heavydelay.BandsSync.Api.model.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@Entity
+@Table(name = "band_members")
+public class BandMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_band_member")
+    private Long idBandMember;
+
+    @ManyToOne
+    @JoinColumn(name = "id_users", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "id_bands", nullable = false)
+    private Band band;
+
+    @ManyToOne
+    @JoinColumn(name = "id_role", nullable = false)
+    private Role role;
+
+    @Column(name = "is_admin", nullable = false)
+    @Builder.Default
+    private Boolean isAdmin = false;
+
+    @CreationTimestamp
+    @Column(name = "join_date", nullable = false, updatable = false)
+    private LocalDateTime joinDate;
+
+    @Column(name = "leave_date", nullable = true)
+    private LocalDateTime leaveDate;    
+
+}
