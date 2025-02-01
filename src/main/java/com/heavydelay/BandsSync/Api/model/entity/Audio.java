705cb7fdc8a86bb5932d4d1d0f6a0f24fdc85196
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-31 22:06:22 -0300

    create audio entity

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/entity/Audio.java
@@ -0,0 +1,41 @@
+package com.heavydelay.BandsSync.Api.model.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@Entity
+@Table(name = "audios")
+public class Audio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_audio")
+    private Long idAudio;
+
+    @Column(name = "format", nullable = false)
+    private String format;
+
+    @Column(name = "file_path", nullable = false)
+    private String filePath;
+
+    @Column(name = "size", nullable = false)
+    private Long size;
+
+    @Column(name = "bitrate", nullable = false)
+    private Short bitrate;
+
+    @CreationTimestamp
+    @Column(name = "upload_date", nullable = false, updatable = false)
+    private Long uploadDate;
+}
