5e60f7e0723731a552a721ec9e91deb6e803100e
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-01-31 22:35:04 -0300

    create audio request dto

---
+++ src/main/java/com/heavydelay/BandsSync/Api/model/dto/audio/AudioRequestDto.java
@@ -0,0 +1,23 @@
+package com.heavydelay.BandsSync.Api.model.dto.audio;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class AudioRequestDto {
+
+    @NotBlank(message = "Format must not be blank")
+    @Size(min = 2, max = 10, message = "Format It must not exceed 10 characters and must have a minimum of 2")
+    private String format;
+
+    @NotBlank(message = "File path must not be blank")
+    @Size(max = 255, message = "File path must have a maximum of 255 characters")
+    private String filePath;
+
+    @NotNull(message = "Size must not be empty")
+    private Long size;
+
+    @NotNull(message = "Bitrate must not be empty")
+    private Short bitrate;
+
+}
