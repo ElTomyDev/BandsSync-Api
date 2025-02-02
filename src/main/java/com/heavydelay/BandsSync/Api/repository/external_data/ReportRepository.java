4bad429d28c398827a8f5841570e530d1bc462fb
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:47:07 -0300

    Create ReportRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/ReportRepository.java
@@ -0,0 +1,9 @@
+package com.heavydelay.BandsSync.Api.repository.external_data;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.heavydelay.BandsSync.Api.model.entity.Report;
+
+public interface ReportRepository extends CrudRepository<Report, Long>{
+
+}
