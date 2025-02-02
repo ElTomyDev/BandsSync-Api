36cde10cbc1efc18a0366ebcb64c7b509f66070a
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:30:16 -0300

    Create AlbumRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/musical_resources/AlbumRepository.java
@@ -0,0 +1,9 @@
+package com.heavydelay.BandsSync.Api.repository.musical_resources;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.heavydelay.BandsSync.Api.model.entity.Album;
+
+public interface AlbumRepository extends CrudRepository<Album, Long>{
+
+}
