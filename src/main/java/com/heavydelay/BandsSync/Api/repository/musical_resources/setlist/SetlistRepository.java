68310964cfd2e7032c5696314393be093d38c52a
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:31:37 -0300

    Create SetlistRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/musical_resources/setlist/SetlistRepository.java
@@ -0,0 +1,9 @@
+package com.heavydelay.BandsSync.Api.repository.musical_resources.setlist;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.heavydelay.BandsSync.Api.model.entity.Setlist;
+
+public interface SetlistRepository extends CrudRepository<Setlist, Long>{
+
+}
