c022c0d424b6bd4cea6901bdaf896209694fd862
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:02:32 -0300

    Update UserRepository.java

--- src/main/java/com/heavydelay/BandsSync/Api/repository/UserRepository.java
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.heavydelay.BandsSync.Api.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.heavydelay.BandsSync.Api.model.entity.User;
+
 public interface UserRepository extends CrudRepository<User, Long>{
     
 }
