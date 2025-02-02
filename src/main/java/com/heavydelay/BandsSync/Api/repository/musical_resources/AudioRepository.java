15d649a5bee8b8819ab6d8eb9d9913e7dab454c4
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:36:26 -0300

    Update AudioRepository.java

--- src/main/java/com/heavydelay/BandsSync/Api/repository/musical_resources/AudioRepository.java
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/musical_resources/AudioRepository.java
@@ -2,6 +2,8 @@ package com.heavydelay.BandsSync.Api.repository.musical_resources;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.heavydelay.BandsSync.Api.model.entity.Audio;
+
 public interface AudioRepository extends CrudRepository<Audio, Long>{
 
 }
