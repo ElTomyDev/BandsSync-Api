d19a4da73c4f0ebc503e24c7281e1c9313cf99fd
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:35:13 -0300

    Create AudioRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/musical_resources/AudioRepository.java
@@ -0,0 +1,7 @@
+package com.heavydelay.BandsSync.Api.repository.musical_resources;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AudioRepository extends CrudRepository<Audio, Long>{
+
+}
