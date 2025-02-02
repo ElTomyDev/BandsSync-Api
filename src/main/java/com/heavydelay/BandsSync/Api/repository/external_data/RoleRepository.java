0ac0f2b60850a9a5b795c3fd0727012431143143
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:43:09 -0300

    Create RoleRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.heavydelay.BandsSync.Api.repository.external_data;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Integer>{
+
+}
