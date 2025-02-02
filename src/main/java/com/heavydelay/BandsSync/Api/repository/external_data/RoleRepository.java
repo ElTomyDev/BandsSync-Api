20656fe9df5ba42144e29559cd081e42bdedd872
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:43:36 -0300

    Update RoleRepository.java

--- src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/RoleRepository.java
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/RoleRepository.java
@@ -2,6 +2,8 @@ package com.heavydelay.BandsSync.Api.repository.external_data;
 
 import org.springframework.data.repository.CrudRepository;
 
+import com.heavydelay.BandsSync.Api.model.entity.Role;
+
 public interface RoleRepository extends CrudRepository<Role, Integer>{
 
 }
