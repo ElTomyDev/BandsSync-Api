8e1a867e79ff9682b84dc515af52213b33a8c8ff
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:01:46 -0300

    Create UserRepository.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.heavydelay.BandsSync.Api.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long>{
+    
+}
