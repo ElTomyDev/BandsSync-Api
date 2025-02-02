cac59dee69ab7148ea40cc22ac9ae0b846d3f2ef
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-02 19:43:21 -0300

    Update GenderRepository.java

--- src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/GenderRepository.java
+++ src/main/java/com/heavydelay/BandsSync/Api/repository/external_data/GenderRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.heavydelay.BandsSync.Api.model.entity.Gender;
 
-public interface GenderRepository extends CrudRepository<Gender, Long>{
+public interface GenderRepository extends CrudRepository<Gender, Integer>{
 
 }
