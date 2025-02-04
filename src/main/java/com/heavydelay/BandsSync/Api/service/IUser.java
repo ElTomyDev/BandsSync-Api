018af69b137a21d60b2a0440a20e278429071d4d
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-04 15:50:29 -0300

    Update user Interfaze service

--- src/main/java/com/heavydelay/BandsSync/Api/service/IUser.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/IUser.java
@@ -2,11 +2,26 @@ package com.heavydelay.BandsSync.Api.service;
 
 import java.util.List;
 
+import com.heavydelay.BandsSync.Api.model.dto.user.UserRequestDto;
 import com.heavydelay.BandsSync.Api.model.dto.user.UserResponseDto;
 
 public interface IUser {
     List<UserResponseDto> showAllUsers(boolean detailed);
     UserResponseDto showUserById(boolean detailed);
     
+    void deleteUserById(Long id);
+
+    UserResponseDto registerNewUser(UserRequestDto dto);
+    UserResponseDto loginUser(UserRequestDto dto);
+
+    // actualizar valore por id o nombre de usuario
+    UserResponseDto updateImageURL(UserRequestDto dto, Long id, String username);
+    UserResponseDto updateName(UserRequestDto dto, Long id, String username);
+    UserResponseDto updateLastname(UserRequestDto dto, Long id, String username);
+    UserResponseDto updateDescription(UserRequestDto dto, Long id, String username);
+    UserResponseDto updateStatus(UserRequestDto dto, Long id, String username);
+    UserResponseDto updatePhoneNumber(UserRequestDto dto, Long id, String username);
+    UserResponseDto updateFindBands(UserRequestDto dto, Long id, String username);
+
 
 }
