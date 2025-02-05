c312db7789cf31b05976ef1b318dae8127fbfc4a
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-05 15:03:23 -0300

    finish endpoints

--- src/main/java/com/heavydelay/BandsSync/Api/model/dto/user/UserRequestDto.java
+++ src/main/java/com/heavydelay/BandsSync/Api/model/dto/user/UserRequestDto.java
@@ -9,6 +9,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.ToString;
 
 /*
     Falta agregar las validaciones personalizadas para el phoneNumber, username, imageUrl y status
@@ -16,54 +17,61 @@ import lombok.Data;
 */
 
 @Data
+@ToString
 public class UserRequestDto {
 
     public interface RegisterView {}
+    public interface OtherView {}
 
     // aca faltan las claves foraneas roles, locations, social_links.
+    @JsonView(OtherView.class)
     @Size(max = 255, message = "Image URL cannot exceed 255 characters")
     private String imageURL;
     
-    @JsonView({RegisterView.class})
-    @NotBlank(message = "Name cannot be blank")
-    @Size(max = 100, message = "Name cannot exceed 100 characters")
+    @JsonView(RegisterView.class)
+    @NotBlank(groups = {RegisterView.class}, message = "Name cannot be blank")
+    @Size(groups = {RegisterView.class}, max = 100, message = "Name cannot exceed 100 characters")
     private String name;
 
-    @JsonView({RegisterView.class})
-    @NotBlank(message = "Last name cannot be blank")
-    @Size(max = 100, message = "Last name cannot exceed 100 characters")
+    @JsonView(RegisterView.class)
+    @NotBlank(groups = {RegisterView.class}, message = "Last name cannot be blank")
+    @Size(groups = {RegisterView.class}, max = 100, message = "Last name cannot exceed 100 characters")
     private String lastname;
 
-    @JsonView({RegisterView.class})
-    @NotBlank(message = "Username cannot be blank")
-    @Size(min = 4, max = 50, message = "Username cannot exceed 50 characters and cannot be less than 4 characters")
+    @JsonView(RegisterView.class)
+    @NotBlank(groups = {RegisterView.class}, message = "Username cannot be blank")
+    @Size(groups = {RegisterView.class}, min = 4, max = 50, message = "Username cannot exceed 50 characters and cannot be less than 4 characters")
     private String username;
 
-    
+    @JsonView(OtherView.class)
     @Size(max = 360, message = "Description cannot exceed 360 characters")
     private String description;
 
+    @JsonView(OtherView.class)
     @NotNull(message = "User status cannot be null")
     private UserStatus status;
 
+    @JsonView(OtherView.class)
     @Size(max = 25, message = "Phone number cannot exceed 25 characters")
     private String phoneNumber;
 
+    @JsonView(OtherView.class)
     @NotNull(message = "Find bands field cannot be null")
     private Boolean findBands;
 
+    @JsonView(OtherView.class)
     @NotNull(message = "Failed login attempts cannot be null")
     @Min(value = 0, message = "Failed login attempts cannot be negative")
     private Integer failedLoginAttempts;
 
-    @JsonView({RegisterView.class})
-    @NotBlank(message = "Password cannot be blank")
-    @Size(min = 6, max = 255, message = "Password cannot exceed 255 characters and cannot be less than 6 characters")
+    @JsonView(RegisterView.class)
+    @NotBlank(groups = {RegisterView.class}, message = "Password cannot be blank")
+    @Size(groups = {RegisterView.class}, min = 6, max = 255, message = "Password cannot exceed 255 characters and cannot be less than 6 characters")
     private String password;
 
-    @JsonView({RegisterView.class})
-    @NotBlank(message = "Email cannot be blank")
-    @Email(message = "It must be a valid email")
+    @JsonView(RegisterView.class)
+    @NotBlank(groups = {RegisterView.class}, message = "Email cannot be blank")
+    @Email(groups = {RegisterView.class}, message = "It must be a valid email")
     private String email;
 
 
