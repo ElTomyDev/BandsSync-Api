d426f76cb3f25d1bd15b3bbd280899099dbc07e0
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-08 07:22:27 -0300

    update location request

--- src/main/java/com/heavydelay/BandsSync/Api/model/dto/external_data/location/LocationRequestDto.java
+++ src/main/java/com/heavydelay/BandsSync/Api/model/dto/external_data/location/LocationRequestDto.java
@@ -1,28 +1,48 @@
 package com.heavydelay.BandsSync.Api.model.dto.external_data.location;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
 
+@Data
+@Builder
+@ToString
 public class LocationRequestDto {
 
-    @NotBlank(message = "Country cannot be blank")
-    @Size(min = 3, max = 100, message = "Country cannot exceed 100 characters and cannot be less than 3 characters")
+    public interface CountryView {}
+    public interface StateView {}
+    public interface CityView {}
+    public interface PostalCodeView {}
+    public interface AddressView {}
+    public interface LocationView extends CountryView, StateView, CityView, PostalCodeView, AddressView {}
+
+    @JsonView(CountryView.class)
+    @NotBlank(groups = {CountryView.class}, message = "Country cannot be blank")
+    @Size(groups = {CountryView.class}, min = 3, max = 100, message = "Country cannot exceed 100 characters and cannot be less than 3 characters")
     private String country;
-
-    @NotBlank(message = "State cannot be blank")
-    @Size(min = 2, max = 100, message = "State cannot exceed 100 characters and cannot be less than 2 characters")
+    
+    @JsonView(StateView.class)
+    @NotBlank(groups = {StateView.class}, message = "State cannot be blank")
+    @Size(groups = {StateView.class}, min = 2, max = 100, message = "State cannot exceed 100 characters and cannot be less than 2 characters")
     private String state;
-
-    @NotBlank(message = "City cannot be blank")
-    @Size(min = 2, max = 100, message = "City cannot exceed 100 characters and cannot be less than 2 characters")
+    
+    @JsonView(CityView.class)
+    @NotBlank(groups = {CityView.class}, message = "City cannot be blank")
+    @Size(groups = {CityView.class}, min = 2, max = 100, message = "City cannot exceed 100 characters and cannot be less than 2 characters")
     private String city;
-
-    @NotBlank(message = "Postal code cannot be blank")
-    @Size(min = 4, max = 20, message = "Postal code cannot exceed 20 characters and cannot be less than 4 characters")
+    
+    @JsonView(PostalCodeView.class)
+    @NotBlank(groups = {PostalCodeView.class}, message = "Postal code cannot be blank")
+    @Size(groups = {PostalCodeView.class}, min = 4, max = 20, message = "Postal code cannot exceed 20 characters and cannot be less than 4 characters")
     private String postalCode;
-
-    @NotBlank(message = "Addres cannot be blank")
-    @Size(min = 5, max = 255, message = "Addres cannot exceed 255 characters and cannot be less than 5 characters")
+    
+    @JsonView(AddressView.class)
+    @NotBlank(groups = {AddressView.class}, message = "Addres cannot be blank")
+    @Size(groups = {AddressView.class}, min = 5, max = 255, message = "Addres cannot exceed 255 characters and cannot be less than 5 characters")
     private String address;
     
 }
