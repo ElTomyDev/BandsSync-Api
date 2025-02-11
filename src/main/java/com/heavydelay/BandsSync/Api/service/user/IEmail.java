ce76b22bec671d49c7fad47aef103db3b6a5c136
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-11 19:10:40 -0300

    finished email service

--- src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
@@ -1,9 +1,8 @@
 package com.heavydelay.BandsSync.Api.service.user;
 
-import com.heavydelay.BandsSync.Api.model.dto.user.user_email.UserEmailRequestDto;
-import com.heavydelay.BandsSync.Api.model.entity.UserEmail;
+import com.heavydelay.BandsSync.Api.model.entity.User;
 
 public interface IEmail {
-    UserEmail createEmail(UserEmailRequestDto dto, String username, Long id);
-    UserEmail updateEmail(UserEmailRequestDto dto, String username, Long id);
+    void createEmail(User user, String email);
+    void updateEmail(String oldEmail, String newEmail, User user);
 }
--- src/main/java/com/heavydelay/BandsSync/Api/service/user/impl/EmailService.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/user/impl/EmailService.java
@@ -3,18 +3,44 @@ package com.heavydelay.BandsSync.Api.service.user.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.heavydelay.BandsSync.Api.exception.ResourceNotFoundException;
+import com.heavydelay.BandsSync.Api.model.entity.User;
+import com.heavydelay.BandsSync.Api.model.entity.UserEmail;
 import com.heavydelay.BandsSync.Api.model.mapper.IUserEmailMapper;
 import com.heavydelay.BandsSync.Api.repository.user.UserEmailRepository;
+import com.heavydelay.BandsSync.Api.service.user.IEmail;
 
 @Service
-public class EmailService {
+public class EmailService implements IEmail{
 
     @Autowired
     UserEmailRepository emailRepository;
     IUserEmailMapper emailMapper;
 
 
-    
+    @Override
+    public void createEmail(User user, String email) {
+        UserEmail userEmail = UserEmail.builder()
+                              .user(user)
+                              .email(email) 
+                              .build();
+                              
+        emailRepository.save(userEmail);
+    }
 
+    @Override
+    public void updateEmail(String oldEmail, String newEmail, User user) {
+
+        UserEmail userEmail = emailRepository.findByIdUser(user.getIdUser()).orElseThrow(
+            () -> new ResourceNotFoundException("The user password with ID '" + user.getIdUser() + "' not found.")
+        );
+
+        if(!userEmail.getEmail().equalsIgnoreCase(oldEmail)){
+            throw new IllegalArgumentException("The current email is not correct");
+        }
+
+        userEmail.setEmail(newEmail);
+        emailRepository.save(userEmail);
+    }
 
 }
