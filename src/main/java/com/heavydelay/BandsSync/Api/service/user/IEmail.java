2b471be782957c8ee09a6288edfd7c8dc327d249
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-11 18:44:28 -0300

    Update IEmail.java

--- src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
@@ -1,7 +1,7 @@
 package com.heavydelay.BandsSync.Api.service.user;
 
-import com.heavydelay.BandsSync.Api.model.dto.user.UserResponseDto;
 import com.heavydelay.BandsSync.Api.model.dto.user.user_email.UserEmailRequestDto;
+import com.heavydelay.BandsSync.Api.model.entity.UserEmail;
 
 public interface IEmail {
     UserEmail updateEmail(UserEmailRequestDto dto, String username, Long id);
