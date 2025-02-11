1b89a7f67621c9f800855196f2e96d56f9603176
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-11 18:50:56 -0300

    create email method

--- src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/user/IEmail.java
@@ -4,5 +4,6 @@ import com.heavydelay.BandsSync.Api.model.dto.user.user_email.UserEmailRequestDt
 import com.heavydelay.BandsSync.Api.model.entity.UserEmail;
 
 public interface IEmail {
+    UserEmail createEmail(UserEmailRequestDto dto, String username, Long id);
     UserEmail updateEmail(UserEmailRequestDto dto, String username, Long id);
 }
