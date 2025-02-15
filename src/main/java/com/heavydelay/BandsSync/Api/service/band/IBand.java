656f69537a2b730c443a34e956701b39d55aea4c
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-02-14 21:49:15 -0300

    create interface for band service

---
+++ src/main/java/com/heavydelay/BandsSync/Api/service/band/IBand.java
@@ -0,0 +1,27 @@
+package com.heavydelay.BandsSync.Api.service.band;
+
+import com.heavydelay.BandsSync.Api.model.dto.band.BandRequestDto;
+import com.heavydelay.BandsSync.Api.model.dto.band.BandResponseDto;
+import com.heavydelay.BandsSync.Api.model.entity.Band;
+
+public interface IBand {
+
+    // CREAR UNA BANDA
+    BandResponseDto createBand(BandRequestDto dto);
+
+    // ELIMINAR BANDA
+    void deleteBand(String bandName, Long id);
+
+    // METODOS UPDATE
+    BandResponseDto updateGender(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateSocialLinks(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateImageUrl(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateBandName(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateAccessCode(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateIsSolist(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateFindMembers(String bandName, Long id, BandRequestDto dto);
+    BandResponseDto updateBiography(String bandName, Long id, BandRequestDto dto);
+
+    // AUXILIARES
+    Band findBandByIdOrBandname(String bandName, Long id);
+}
