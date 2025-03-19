541ac5a66e29f7fe70932ab0c3740a2353fd33f6
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-03-19 08:08:00 -0300

    Finish setlist interface service

--- src/main/java/com/heavydelay/BandsSync/Api/service/musical_resources/setlist/ISetlist.java
+++ src/main/java/com/heavydelay/BandsSync/Api/service/musical_resources/setlist/ISetlist.java
@@ -2,11 +2,18 @@ package com.heavydelay.BandsSync.Api.service.musical_resources.setlist;
 
 import java.util.List;
 
+import com.heavydelay.BandsSync.Api.model.dto.musical_resources.setlist.SetlistRequestDto;
 import com.heavydelay.BandsSync.Api.model.dto.musical_resources.setlist.SetlistResponseDto;
 
 public interface ISetlist {
     List<SetlistResponseDto> showAllSetlist(Long bandId, Boolean inUse, boolean detailed);
     SetlistResponseDto showSetlist(Long idSetlist, Long idBand, String setlistName, boolean detailed);
 
+    SetlistResponseDto createSetlist(Long idBand, SetlistRequestDto dto);
     
+    void deteleSetlist(Long idSetlist, Long idBand, String setlistName);
+
+    SetlistResponseDto updateSetlistName(Long idSetlist, Long idBand, String setlistName, SetlistRequestDto dto);
+    SetlistResponseDto updateInUse(Long idSetlist, Long idBand, String setlistName, SetlistRequestDto dto);
+
 }
