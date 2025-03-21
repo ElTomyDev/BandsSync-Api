abc3b42d833ff5f214647338da585130060eca45
Tomas Franco Gimenes <eltomyprog@gmail.com>
2025-03-21 09:36:02 -0300

    Create SetlistController.java

---
+++ src/main/java/com/heavydelay/BandsSync/Api/controller/musical_resources/setlist/SetlistController.java
@@ -0,0 +1,10 @@
+package com.heavydelay.BandsSync.Api.controller.musical_resources.setlist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/setlist")
+public class SetlistController {
+    
+}
